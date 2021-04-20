8be38c23ddc1e197e2ad4ec4bee87e7c686fbca8
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-04-20 18:00:20 -0300

    definindo controle transacional Spring Data JPA

--- src/main/java/br/com/casadocodigo/ApicdcApplication.java
+++ src/main/java/br/com/casadocodigo/ApicdcApplication.java
@@ -2,8 +2,10 @@ package br.com.casadocodigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class ApicdcApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/casadocodigo/detalhelivro/CrudAutoresController.java
+++ src/main/java/br/com/casadocodigo/detalhelivro/CrudAutoresController.java
@@ -1,24 +1,23 @@
 package br.com.casadocodigo.detalhelivro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
 public class CrudAutoresController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private AutorRepository autorRepository;
 
     @PostMapping("/api/autor")
     @Transactional
     public void novo(@Valid @RequestBody NovoAutorForm form) {
         Autor novoAutor = form.novoAutor();
-        manager.persist(novoAutor);
+        autorRepository.save(novoAutor);
     }
 }
